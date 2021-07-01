888e2b3055426d0fee058660a3d80e81dce7d0e8
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2021-07-01 17:08:36 +0200

    Refactor parseMessageBlock

--- src/parser/message/parse-message-block.ts
+++ src/parser/message/parse-message-block.ts
@@ -9,6 +9,43 @@ import { TokenAccessor } from '../token-accessor'
 import { MessageBlock } from './message-description'
 
 /**
+ * This class helps construct a MessageBlock from activations and a return value.
+ * Note that invocation order is important; there cannot be any further activations added after a return value was set.
+ */
+class MessageBlockBuilder {
+  private readonly activations: Activation[] = []
+  private returnValue: string | undefined
+  private returnEvidence: Token | undefined
+
+  private ensureNotReturned (evidence: Token): void {
+    if (this.returnValue != null) {
+      throw new AlreadyReturnedError(evidence)
+    }
+  }
+
+  applyReturn (value: string, evidence: Token): void {
+    this.ensureNotReturned(evidence)
+    this.returnValue = value
+    this.returnEvidence = evidence
+  }
+
+  addActivation (activation: Activation, evidence: Token): void {
+    this.ensureNotReturned(evidence)
+    this.activations.push(activation)
+  }
+
+  build (blockClose: Token): MessageBlock {
+    return {
+      activations: this.activations,
+      returnValue: this.returnValue,
+      evidence: {
+        returnValue: this.returnEvidence ?? blockClose
+      }
+    }
+  }
+}
+
+/**
  * Determine whether the given token marks the beginning of a message block.
  * This will only produce valid results when right at the end of a message description.
  *
@@ -36,34 +73,19 @@ export function detectMessageBlock (token: Token): boolean {
 export function parseMessageBlock (tokens: TokenAccessor, entities: EntityLookup, active: Entity): MessageBlock {
   tokens.pop(TokenType.BLOCK_LEFT)
 
-  const activations: Activation[] = []
-
-  let returnValue: string | undefined
-  let tReturn: Token | undefined
+  const builder = new MessageBlockBuilder()
 
   let tBlockRight
   while ((tBlockRight = tokens.popOptional(TokenType.BLOCK_RIGHT)) == null) {
     const token = tokens.peek()
-    if (returnValue != null) {
-      throw new AlreadyReturnedError(token)
-    }
     if (detectMessage(token)) {
-      activations.push(parseMessage(tokens, entities, active))
-      continue
+      builder.addActivation(parseMessage(tokens, entities, active), token)
+    } else if (detectReturn(token)) {
+      builder.applyReturn(parseReturn(tokens), token)
+    } else {
+      throw new UnexpectedTokenError(token)
     }
-    if (detectReturn(token)) {
-      returnValue = parseReturn(tokens)
-      tReturn = token
-      continue
-    }
-    throw new UnexpectedTokenError(token)
   }
 
-  return {
-    activations,
-    returnValue,
-    evidence: {
-      returnValue: tReturn ?? tBlockRight
-    }
-  }
+  return builder.build(tBlockRight)
 }
