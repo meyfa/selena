6b96f98572b11cba796c4512cd9a1be0799da9f8
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2021-07-05 13:01:21 +0200

    Add unit tests for geometry classes

---
+++ test/util/geometry/point.test.ts
@@ -0,0 +1,76 @@
+import { Point } from '../../../src/util/geometry/point'
+
+import { expect } from 'chai'
+
+describe('src/util/geometry/point.ts', function () {
+  describe('ORIGIN', function () {
+    it('is the point (0,0)', function () {
+      expect(Point.ORIGIN).to.be.an.instanceOf(Point)
+      expect(Point.ORIGIN.x).to.equal(0)
+      expect(Point.ORIGIN.y).to.equal(0)
+    })
+  })
+
+  describe('constructor', function () {
+    it('stores x, y values as given', function () {
+      expect(new Point(42.25, 37.75).x).to.equal(42.25)
+      expect(new Point(42.25, 37.75).y).to.equal(37.75)
+      expect(new Point(-42.25, 37.75).x).to.equal(-42.25)
+      expect(new Point(42.25, -37.75).y).to.equal(-37.75)
+    })
+  })
+
+  describe('#translate()', function () {
+    it('adds the given deltas to the coordinates', function () {
+      expect(new Point(10, -15).translate(12.25, 17.75).x).to.equal(22.25)
+      expect(new Point(10, -15).translate(12.25, 17.75).y).to.equal(2.75)
+      expect(new Point(10, 15).translate(-12.25, -17.75).x).to.equal(-2.25)
+      expect(new Point(10, 15).translate(-12.25, -17.75).y).to.equal(-2.75)
+    })
+
+    it('does not modify the original point', function () {
+      const obj = new Point(42, 37)
+      obj.translate(3, 3)
+      expect(obj.x).to.equal(42)
+      expect(obj.y).to.equal(37)
+    })
+  })
+
+  describe('#withX()', function () {
+    it('sets the x coordinate', function () {
+      expect(new Point(10, -15).withX(42).x).to.equal(42)
+      expect(new Point(10, -15).withX(-42).x).to.equal(-42)
+    })
+
+    it('keeps the y coordinate', function () {
+      expect(new Point(10, -15).withX(42).y).to.equal(-15)
+      expect(new Point(10, -15).withX(-42).y).to.equal(-15)
+    })
+
+    it('does not modify the original point', function () {
+      const obj = new Point(42, 37)
+      obj.withX(3)
+      expect(obj.x).to.equal(42)
+      expect(obj.y).to.equal(37)
+    })
+  })
+
+  describe('#withY()', function () {
+    it('sets the y coordinate', function () {
+      expect(new Point(10, -15).withY(42).y).to.equal(42)
+      expect(new Point(10, -15).withY(-42).y).to.equal(-42)
+    })
+
+    it('keeps the x coordinate', function () {
+      expect(new Point(10, -15).withY(42).x).to.equal(10)
+      expect(new Point(10, -15).withY(-42).x).to.equal(10)
+    })
+
+    it('does not modify the original point', function () {
+      const obj = new Point(42, 37)
+      obj.withY(3)
+      expect(obj.x).to.equal(42)
+      expect(obj.y).to.equal(37)
+    })
+  })
+})
---
+++ test/util/geometry/size.test.ts
@@ -0,0 +1,52 @@
+import { Size } from '../../../src/util/geometry/size'
+
+import { expect } from 'chai'
+
+describe('src/util/geometry/size.ts', function () {
+  describe('ZERO', function () {
+    it('is a Size with zero width and zero height', function () {
+      expect(Size.ZERO).to.be.an.instanceOf(Size)
+      expect(Size.ZERO.width).to.equal(0)
+      expect(Size.ZERO.height).to.equal(0)
+    })
+  })
+
+  describe('constructor', function () {
+    it('stores width, height values', function () {
+      expect(new Size(42.25, 37.75).width).to.equal(42.25)
+      expect(new Size(42.25, 37.75).height).to.equal(37.75)
+      expect(new Size(0, 0).width).to.equal(0)
+      expect(new Size(0, 0).height).to.equal(0)
+    })
+
+    it('sets negative values to 0', function () {
+      expect(new Size(0, 37.75).width).to.equal(0)
+      expect(new Size(0, 37.75).height).to.equal(37.75)
+      expect(new Size(42.25, 0).width).to.equal(42.25)
+      expect(new Size(42.25, 0).height).to.equal(0)
+    })
+  })
+
+  describe('#add()', function () {
+    it('adds the given deltas to the dimensions', function () {
+      expect(new Size(10, 0).add(3, 7.75).width).to.equal(13)
+      expect(new Size(10, 0).add(3, 7.75).height).to.equal(7.75)
+      expect(new Size(10, 15).add(-3, -7.75).width).to.equal(7)
+      expect(new Size(10, 15).add(-3, -7.75).height).to.equal(7.25)
+    })
+
+    it('sets negative result values to 0', function () {
+      expect(new Size(10, 0).add(-12.25, 7.75).width).to.equal(0)
+      expect(new Size(10, 0).add(-12.25, 7.75).height).to.equal(7.75)
+      expect(new Size(10, 15).add(-2.25, -17.75).width).to.equal(7.75)
+      expect(new Size(10, 15).add(-2.25, -17.75).height).to.equal(0)
+    })
+
+    it('does not modify the original size', function () {
+      const obj = new Size(42, 37)
+      obj.add(3, 3)
+      expect(obj.width).to.equal(42)
+      expect(obj.height).to.equal(37)
+    })
+  })
+})
