f1319a82bd3c18aa389b5c26a3ce7449897e71ca
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2021-07-12 20:37:08 +0200

    feat(diagram): Add ActivationBarDrawable, ActivationBarDiagramPart (#24)

---
+++ src/diagram/drawables/activation-bar-drawable.ts
@@ -0,0 +1,60 @@
+import { Size } from '../../util/geometry/size'
+import { RenderAttributes, Renderer } from '../../renderer/renderer'
+import { Drawable } from './drawable'
+import { BoxDrawable } from './box-drawable'
+import { Point } from '../../util/geometry/point'
+
+/**
+ * A Drawable that draws a simple box, but offers easier configuration for purposes of rendering activation bars
+ * as compared to a regular box drawable.
+ */
+export class ActivationBarDrawable implements Drawable {
+  private readonly thickness: number
+  private topLeft: Point = Point.ORIGIN
+
+  private readonly box: BoxDrawable
+
+  constructor (thickness: number) {
+    this.thickness = thickness
+    this.box = new BoxDrawable(new Size(this.thickness, 0))
+  }
+
+  /**
+   * Position the drawable horizontally, based on the lifeline coordinate and an activation level.
+   * The level specifies how far to "indent" the activation bar.
+   *
+   * @param x The lifeline x coordinate.
+   * @param level The number of activations of the lifeline at this moment.
+   */
+  setHorizontalPosition (x: number, level: number): void {
+    const boxX = x + (level - 2) * (this.thickness / 2)
+    this.topLeft = this.topLeft.withX(boxX)
+  }
+
+  /**
+   * Set the vertical start coordinate of this drawable.
+   *
+   * @param y The start coordinate.
+   */
+  setTopOffset (y: number): void {
+    this.topLeft = this.topLeft.withY(y)
+  }
+
+  /**
+   * Set the (vertical) length of this drawable.
+   *
+   * @param length The length (y extension from top edge).
+   */
+  setLength (length: number): void {
+    this.box.setSize(new Size(this.thickness, length))
+  }
+
+  draw (renderer: Renderer): void {
+    this.box.setTopLeft(this.topLeft)
+    this.box.draw(renderer)
+  }
+
+  measure (attr: RenderAttributes): Size {
+    return this.box.measure(attr)
+  }
+}
---
+++ src/diagram/parts/activation-bar-diagram-part.ts
@@ -0,0 +1,67 @@
+import { Renderer } from '../../renderer/renderer'
+import { ActivationBarDrawable } from '../drawables/activation-bar-drawable'
+import { ACTIVATION_THICKNESS } from '../config'
+import { DiagramPart } from './diagram-part'
+
+/**
+ * A diagram part representing an activation bar.
+ *
+ * The bar is located on a particular lifeline, at a given level (number of "active activations"; indentation level).
+ * Its vertical extent (start and end y coordinates) is defined to be between two messages.
+ * The start message id has to be specified during construction, the end message id can be set later.
+ * Top and bottom edge positions have to be computed externally, based on this information.
+ */
+export class ActivationBarDiagramPart implements DiagramPart {
+  readonly startMessageId: number
+  endMessageId: number | undefined
+  readonly entityId: string
+  readonly level: number
+
+  private readonly drawable: ActivationBarDrawable
+
+  private top: number = 0
+  private bottom: number = 0
+  private lifelineX: number = 0
+
+  constructor (startMessageId: number, entityId: string, level: number) {
+    this.startMessageId = startMessageId
+    this.entityId = entityId
+    this.level = level
+
+    this.drawable = new ActivationBarDrawable(ACTIVATION_THICKNESS)
+  }
+
+  /**
+   * Set the y coordinate of the start message.
+   *
+   * @param top The y coordinate where the activation bar will be positioned.
+   */
+  setTop (top: number): void {
+    this.top = top
+  }
+
+  /**
+   * Set the y coordinate of the end message.
+   *
+   * @param bottom The y coordinate where the activation bar will end.
+   */
+  setBottom (bottom: number): void {
+    this.bottom = bottom
+  }
+
+  /**
+   * Set the x position of the associated lifeline.
+   *
+   * @param x The lifeline x coordinate.
+   */
+  setLifelineX (x: number): void {
+    this.lifelineX = x
+  }
+
+  draw (renderer: Renderer): void {
+    this.drawable.setTopOffset(this.top)
+    this.drawable.setHorizontalPosition(this.lifelineX, this.level)
+    this.drawable.setLength(this.bottom - this.top)
+    this.drawable.draw(renderer)
+  }
+}
